964d56edccea57a7677cc06860db5ee5b33474ff
Sebastiaan <sebastiaan.wouters@student.kuleuven.be>
2023-02-13 18:47:22 +0100

    export global

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from "./relay-pool";
 export * from "./author";
+export * from "./global"
 export * from "./event"
 export * from "./collect";
 export {emitEventsOnNextTick} from "./on-event-filters";
